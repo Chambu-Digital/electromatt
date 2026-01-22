7cbb8976724e2eb14e1be0f0b5b01bd282008be6
Nimmoh <nimmohndabari@gmail.com>
2026-01-23 00:45:45 +0300

    next config channge

--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-<<<<<<< HEAD
-  experimental: {
-    turbo: {
-      root: process.cwd(),
-    },
-  },
-=======
-  // React compiler is now a top-level option in Next.js 16
-  reactCompiler: false,
->>>>>>> 9e6371980dee674cfc1bbaf9d49dc2ffac5847f7
 }
 
 export default nextConfig
